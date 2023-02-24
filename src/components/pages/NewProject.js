84455b3ef64336c317595afc41f4dd56b2f78be3
goesrafa <goes.rafaelasilva@gmail.com>
2023-02-24 17:24:01 -0300

    :memo: Add: history
    implementação da API (ficticia) na parte onde ela é chamada pra adicionar um novo projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,13 +1,35 @@
+import { useHistory } from "react-router-dom";
 import React from "react";
 import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 function NewProject() {
+  const history = useHistory();
+
+  function createPost(project) {
+    //Inicializando costs e os serviços
+    project.costs = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project)
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Novo projeto</h1>
       <p>Crie o seu projeto para posteriormente adicionar seu serviço</p>
-      <ProjectForm btnText='Criar Projeto'/>
+      <ProjectForm btnText="Criar Projeto" />
     </div>
   );
 }
